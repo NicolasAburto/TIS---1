cabd3acf4a06e30bf14731443f7b87545d5f85d1
Raúl Eduardo Burgos Sánchez <71039391+LaLoiDe@users.noreply.github.com>
2021-11-23 20:25:30 -0300

    Update Edificios

--- Entrega 2/Mantenedores/edificios.php
+++ Entrega 2/Mantenedores/edificios.php
@@ -10,41 +10,38 @@
       <link rel="stylesheet" type="text/css" href="../estilo.css">
       <title>Edificios</title></head>
   <body>
-      <table style="width: 500px; text-align: left;">
-        <thead>
-          <tr>
-            <th>Nombre Edificio</th>
-            <th>Capacidad Maxima</th>
-            <th>Operaciones</th>
-            <th><a class="btn btn-dark" href="crear-edificio.php" role="button">Crear</a></th>
-          </tr>
-        </thead>
-        <tbody>
-          <?php
-            $consulta = "SELECT * FROM edificio";
-            $resultado = mysqli_query($conexion, $consulta);
+    <table class="table">
+      <thead class="thead-dark">
+        <tr>
+          <th scope="col">Nombre Edificio</th>
+          <th scope="col">Capacidad Maxima</th>
+          <th scope="col">Operaciones</th>
+          <th><a class="d-inline btn btn-dark" href="crear-edificio.php" role="button">Crear</a></th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+          $consulta = "SELECT * FROM edificio";
+          $resultado = mysqli_query($conexion, $consulta);
 
-            while($row=mysqli_fetch_assoc($resultado)){
-              $id_edificio = $row['id_edificio'];
-              $nombre_edificio = $row['nombre_edificio'];
-              $capacidad_maxima_edificio = $row['capacidad_maxima_edificio'];
-              echo "<tr>";
-                echo "<td>".$nombre_edificio."</td>";
-                echo "<td>".$capacidad_maxima_edificio."</td>";
-                echo "<td>
-                        <form action='eliminar-edificio.php' method='POST'>
-                          <input type='hidden' name='id_edificio' value='".$id_edificio."'>
-                          <button type='submit' style='float:left;margin-right:5px'>Eliminar</button>
-                        </form>
-                        <a href='editar-edificio.php?id_edificio=".$id_edificio."'>
-                          <button>Editar</button>
-                        </a>
-                      </td>";
-              echo "</tr>";
-            }
-          ?>
-        </tbody>
-        </table>
+          while($row=mysqli_fetch_assoc($resultado)){
+            $id_edificio = $row['id_edificio'];
+            $nombre_edificio = $row['nombre_edificio'];
+            $capacidad_maxima_edificio = $row['capacidad_maxima_edificio'];
+            echo "<tr>";
+              echo "<td>".$nombre_edificio."</td>";
+              echo "<td>".$capacidad_maxima_edificio."</td>";
+              echo "<td class='p-0'>
+                      <a class='m-2 btn btn-dark' href='editar-edificio.php?id_edificio=".$id_edificio."' role='button'>Editar</a>
+                      <form class='d-inline' action='eliminar-edificio.php' method='POST'>
+                        <input type='hidden' name='id_edificio' value='".$id_edificio."'>
+                        <button class='m-2 btn btn-dark' type='submit'>Eliminar</button>
+                      </form>
+                    </td>";
+            echo "</tr>";
+          }
+        ?>
+      </tbody>
     </div>
   </body>
 </html>
