ec3af421b74ebd50332406ac3b490cef4e56be1a
cfierrom <55846057+cfierrom@users.noreply.github.com>
2021-11-23 13:34:58 -0300

    Create portal.php

---
+++ Entrega 2/portal.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="estilo.css">
+        <title>Portal - Aforo UCSC</title>
+    </head>
+    <body>
+        <!--Menu-->
+        <ul class="nav justify-content-end">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="portal.php">Inicio</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="envivo.php">En Vivo</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Datos anteriores</a>
+            </li>
+        </ul>
+        <div class="container">
+            <div class="col">
+                <h1>Control de Aforos en edificios</h1>
+                <h5>Medidas de prevencion COVID-19 de la UCSC</h5>
+            </div>
+            <div class="row" align="center">
+                <div class="col">
+                    <button type="button" class="btn btn-outline-secondary">Ver en vivo</button>
+                </div>
+                <div class="col">
+                    <button type="button" class="btn btn-outline-secondary">¿Necesitas ayuda?</button>
+                </div>
+            </div>
+            <div class="row" align="center">
+                <div class="col">
+                    <button type="button" class="btn btn-outline-secondary">Ver datos pasados</button>
+                </div>
+                <div class="col">
+                    <button type="button" class="btn btn-outline-secondary">Reportar problema</button>
+                </div>
+            </div>
+        </div>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    </body>
+</html>
