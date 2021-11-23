3cc2eb2e140fa6b625551e581215667c22aca391
cfierrom <55846057+cfierrom@users.noreply.github.com>
2021-11-23 15:29:11 -0300

    Create envivo.php

---
+++ Entrega 2/envivo.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="estilo.css">
+        <title>Portal - Aforo UCSC</title>
+    </head>
+    <body>
+        <!--Menu-->
+        <ul class="nav justify-content-end">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="portal.php">Inicio</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="envivo.php">En Vivo</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Datos anteriores</a>
+            </li>
+        </ul>
+        <div class="container contenedor">
+            <div class="col">
+                <!--Borrar --cantidad-- -->
+                <h2 name="cantidad">--cantidad--</h2>
+                <h4>Personas actualmente</h4>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <!--Columna 1-->
+                        columna 1
+                    </div>
+                    <div class="col">
+                        <!--Columna 2-->
+                        columna2
+                    </div>
+                </div>
+            </div>
+        </div>
+        <footer>
+            <p>Unidad de Infraestructura DO - UCSC.</p>
+            <a href="https://portal.ucsc.cl/">
+                <label>Ir a Portal Institucional</label>
+            </a>
+        </footer>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    </body>
+</html>
