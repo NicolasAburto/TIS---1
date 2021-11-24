a5b0151d91c7b3fb406dea9ac51f30d54b727e5d
fvalentin1 <71657263+fvalentin1@users.noreply.github.com>
2021-11-24 00:34:20 -0300

    Update editar_personal.php

--- Entrega 2/Mantenedores personal/editar_personal.php
+++ Entrega 2/Mantenedores personal/editar_personal.php
@@ -19,13 +19,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Personal editar</title>
 
+    <link rel="stylesheet" href="estilos_personal.css">
+
      <!-- Bootstrap CSS -->
      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
 
     <div class="container">
-        <div class="row">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <img src="logoucsc.png" alt="" width="100" height="33" class="d-inline-block align-text-top">
+                    Aforo UCSC - Personal
+                </a>
+            </div>
+        </nav>
+
+
+        <div class="row fondo">
+
+
             <div class="col-lg-12 pt-5">
                 <h3>Formulario de Edición de personal</h3>
             </div>
