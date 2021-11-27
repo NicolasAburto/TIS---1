5800be6be5afd4556e1f5242bb94e94209efc4bf
NicolasAburto <88061506+NicolasAburto@users.noreply.github.com>
2021-11-27 16:34:15 -0300

    Delete eliminar-edificio.php

--- Entrega 2/Mantenedores/eliminar-edificio.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require("../conexion.php");
-    
-    $id_edificio = $_POST['id_edificio'];
-    $delete = "DELETE FROM edificio WHERE id_edificio = '$id_edificio'";
-    $resultado = mysqli_query($conexion, $delete);
-    header("Location: edificios.php");
-?>
