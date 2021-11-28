e46093b00983fb5dd4325525347a19ef77c04f28
NicolasAburto <88061506+NicolasAburto@users.noreply.github.com>
2021-11-28 20:13:39 -0300

    Update index.php

--- Entrega 2/index.php
+++ Entrega 2/index.php
@@ -12,6 +12,29 @@
         <title>Aforo UCSC</title>
     </head>
     <body>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="https://www.ucsc.cl/">
+                <img src="CSS/logoucsc.png" alt="" width="100" height="33" class="d-inline-block align-text-top">
+                Aforo UCSC - Administrador
+            </a>
+            <ul class="nav justify-content-end">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="portal.php">Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="envivo.php">En Vivo</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Datos anteriores</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+
+    <div class="fondoacceso">
         <div class="container contenedor-login">
             <div class="col">
                 <div class="row margen">
@@ -29,7 +52,7 @@
                 </div>
             </div>
         </div>
-
+    </div>
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     </body>
