d3aa17fefd3d1b3173bc3ec7a857ced8e2e7f7f8
NicolasAburto <88061506+NicolasAburto@users.noreply.github.com>
2021-11-28 20:13:56 -0300

    Update portal.php

--- Entrega 2/portal.php
+++ Entrega 2/portal.php
@@ -1,54 +1,77 @@
 <!doctype html>
 <html lang="es">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-        <link rel="stylesheet" type="text/css" href="CSS/estilo.css">
-        <title>Portal - Aforo UCSC</title>
-    </head>
-    <body>
-        <!--Menu-->
-        <ul class="nav justify-content-end">
-            <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="portal.php">Inicio</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="envivo.php">En Vivo</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Datos anteriores</a>
-            </li>
-        </ul>
-        <div class="container contenedor">
-            <div class="col">
-                <h1>Control de Aforos en edificios</h1>
-                <h5>Medidas de prevencion COVID-19 de la UCSC</h5>
-            </div>
-            <div class="row" align="center">
-                <div class="col">
-                    <a class="btn btn-outline-secondary _a" href="envivo.php" role="button">Ver en vivo</a>
-                </div>
-                <div class="col">
-                    <a class="btn btn-outline-secondary _a" href="#" role="button">¿Necesitas ayuda?</a>
-                </div>
-            </div>
-            <div class="row" align="center">
-                <div class="col">
-                    <a class="btn btn-outline-secondary _a" href="#" role="button">Ver datos pasados</a>
-                </div>
-                <div class="col">
-                    <a class="btn btn-outline-secondary _a" href="https://ayuda.ucsc.cl/" role="button">Reportar problema</a>
-                </div>
-            </div>
-        </div>
-        <footer>
-            <p>Unidad de Infraestructura DO - UCSC.</p>
-            <a href="portal.ucsc.cl">
-                <label>Ir a Portal Institucional</label>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="CSS/estilo.css">
+    <title>Portal - Aforo UCSC</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="https://www.ucsc.cl/">
+                <img src="CSS/logoucsc.png" alt="" width="100" height="33" class="d-inline-block align-text-top">
+                Aforo UCSC
             </a>
-        </footer>
+            <ul class="nav justify-content-end">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="portal.php">Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="envivo.php">En Vivo</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Datos anteriores</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    
+    <div class="container-fluid fondoportal capacontainer">
+        <div class="row capacontainer">
+            <div class="col-lg-12 col-md-12 col-sm-12">
+                <div class="container contenedor">
+                
+                    <div class="col">
+                        <h1 class="titulo">Control de Aforos en edificios</h1>
+                        <h5 class="subtitulo">Medidas de prevención COVID-19 de la UCSC</h5>
+                    </div>
+                    <div class="row" align="center">
+                        <div class="col">
+                            <a class="btn btn-outline-secondary _a" href="envivo.php" role="button">Ver en vivo</a>
+                        </div>
+                        <div class="col">
+                            <a class="btn btn-outline-secondary _a" href="#" role="button">¿Necesitas ayuda?</a>
+                        </div>
+                    </div>
+                    <div class="row" align="center">
+                        <div class="col">
+                            <a class="btn btn-outline-secondary _a" href="#" role="button">Ver datos pasados</a>
+                        </div>
+                        <div class="col">
+                            <a class="btn btn-outline-secondary _a" href="https://ayuda.ucsc.cl/" role="button">Reportar problema</a>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
 
+            <div class="col-lg-12 col-md-12 col-sm-12">
+                <footer>
+                    <p class="textofooter">Unidad de Infraestructura DO - UCSC. Todos los derechos reservados</p>
+
+                    <a href="https://portal.ucsc.cl/">
+                        <label class="linkfooter">Ir a Portal Institucional</label>
+                    </a>
+                </footer>
+            </div>
+
+
+        </div>
+    </div>
+        
+        
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    </body>
+</body>
 </html>
