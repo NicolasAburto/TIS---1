e37ab0f8776b1a59635809db7f745591349c2178
NicolasAburto <88061506+NicolasAburto@users.noreply.github.com>
2021-11-28 18:16:38 -0300

    Update edificios.php

--- Entrega 2/Mantenedor_edificio/edificios.php
+++ Entrega 2/Mantenedor_edificio/edificios.php
@@ -9,7 +9,7 @@
   <title>Edificios</title></head>
   
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-  <link rel="stylesheet" href="../CSS/estilo_edificio.css">
+  <link rel="stylesheet" href="../CSS/estilo_mantenedor.css">
 </head>
 
 <body>
@@ -25,7 +25,7 @@
     </div>
   </nav>
 
-  <div class="container-fluid fondo">
+  <div class="container-fluid fondoedificio">
     <div class="row">
 
       <div class="col-lg-6 col-md-12 col-sm-12">
@@ -92,4 +92,3 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
-
