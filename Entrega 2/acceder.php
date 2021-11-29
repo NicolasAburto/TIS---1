68d8aa5693431c23fb20f93a5bb1c6c06cf3a6f4
NicolasAburto <88061506+NicolasAburto@users.noreply.github.com>
2021-11-29 19:43:35 -0300

    Update acceder.php

--- Entrega 2/acceder.php
+++ Entrega 2/acceder.php
@@ -1,15 +1,15 @@
 <?php
-    require("conexion.php")
+    require("conexion.php");
 
-    $usuario = $_POST['user'];
+    $usuario = $_POST['input_user'];
 
-    $consulta = "SELECT * FROM personal WHERE run='.$usuario.'";
+    $consulta = "SELECT * FROM personal WHERE run=$usuario";
     $resultado = mysqli_query($conexion,$consulta);
 
     while($row=mysqli_fetch_assoc($resultado)){
         $rol = $row['cargo'];
-        if($rol = "Administrativo"){
-            header('Location: portal.php');
+        if($rol == "Administrativo"){
+            header('Location: index_manten.php');
         }else{
             echo 'El run no es administrador';
         }
