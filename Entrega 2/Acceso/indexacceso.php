45b896dab55fdd709744366a82d38fe7deea8db7
fvalentin1 <71657263+fvalentin1@users.noreply.github.com>
2021-12-02 22:42:41 -0300

    Create indexacceso.php

--- Entrega 2/Acceso/indexacceso.php
+++ Entrega 2/Acceso/indexacceso.php
@@ -2,46 +2,102 @@
     $conexion = mysqli_connect("localhost","root","","aforoucsc2");
 ?>
 
-<!DOCTYPE html>
+<!doctype html>
 <html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <title>Control de Acceso a Edificios UCSC</title>
-</head>
-<body>
-    <h1>Ingreso a un edificio UCSC</h1>
+  <head>
 
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <form action="ingresar.php" method="POST">
-        <h3>Ingrese su rut:</h3>
-        <input type="text" name="rut_acceso" placeholder="123456789">
-        <h3>Seleccione edificio a Acceder:</h3>
-        <input type="text" name="edificio_acceso" placeholder="ID Edificio">
-        <h3>Ingrese fecha de Acceso:</h3>
-        <input type="date" name="fecha_acceso">
-        <h3>Ingrese hora de ingreso al edificio:</h3>
-        <input type="time" name="hora_ingreso">
-        <h3>Ingrese hora de abandono del edificio:</h3>
-        <input type="time" name="hora_salida">
-        <br>
-        <input type="submit" value="Ingresar">
-    </form>
     
-    <hr>
-    <h3>Listado Edificios UCSC</h3>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <?php
-        $consulta="SELECT * FROM edificio";
-        $resultado=mysqli_query($conexion,$consulta);
+    <title>Acceso a un Edificio UCSC - Campus San Andrés</title>
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="https://www.ucsc.cl/">
+                <img src="https://www.ucsc.cl/wp-content/uploads/2015/01/logo_horizontal_color_sinfondo.png" alt="" width="100" height="33" class="d-inline-block align-text-top">
+                Aforo UCSC - Acceso Edificios Campus San Andrés
+            </a>
+            <ul class="nav justify-content-end">
+                <li class="nav-item">
+                    <a class="nav-link active navbarlinks" aria-current="page" href="../index.php">Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link navbarlinks" href="../envivo.php">En Vivo</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link navbarlinks" href="../datosanteriores.php">Datos anteriores</a>
+                </li>
 
-        while($row=mysqli_fetch_assoc($resultado)){
-            $id_recibido = $row["id_edificio"];
-            $nombre_recibido = $row["nombre_edificio"];
-            $capmax_recibido = $row["capacidad_maxima_edificio"];
-            echo "<p>".$id_recibido." ".$nombre_recibido." ".$capmax_recibido."</p>";
-        }
-    ?>
+                <a href="acceso.php">
+                    <span class="material-icons iconousuario text-black">
+                        account_circle
+                    </span>
+                </a>
+            </ul>
+        </div>
+    </nav>
 
+    <div class="container-fluid fondoportal">
+        <div class="row capacontainer">
+            <div class="container-fluid">
+                <form action="ingresar.php" method="POST">
+                    <h3>Ingrese su rut:</h3>
+                    <input type="text" name="rut_acceso" placeholder="123456789">
+                    <h3>Seleccione edificio a Acceder:</h3>
+                    <input type="text" name="edificio_acceso" placeholder="ID Edificio">
+                    <h3>Ingrese fecha de Acceso:</h3>
+                    <input type="date" name="fecha_acceso">
+                    <h3>Ingrese hora de ingreso al edificio:</h3>
+                    <input type="time" name="hora_ingreso">
+                    <h3>Ingrese hora de abandono del edificio:</h3>
+                    <input type="time" name="hora_salida">
+                    <br>
+                    <input type="submit" value="Ingresar">
+                </form>
+                <hr>
+                <?php
+                    $queryenvivo = "SELECT run_personal FROM `puede` WHERE fecha_ingreso = (select CURDATE()) AND hora_ingreso < (SELECT curTime()) AND (SELECT curTime()) < hora_salida";
+                    $resultadoenvivo=mysqli_query($conexion,$queryenvivo);
+                    $numero = mysqli_num_rows($resultadoenvivo);
+                    echo 'Personas actualmente en el campus: ' . $numero;
+                 ?>
+                <hr>
+                <h3>Listado Edificios UCSC</h3>
 
-</body>
+                <?php
+                    $consulta="SELECT * FROM edificio";
+                    $resultado=mysqli_query($conexion,$consulta);
+
+                    while($row=mysqli_fetch_assoc($resultado)){
+                        $id_recibido = $row["id_edificio"];
+                        $nombre_recibido = $row["nombre_edificio"];
+                        $capmax_recibido = $row["capacidad_maxima_edificio"];
+                        echo "<p>".$id_recibido." ".$nombre_recibido." ".$capmax_recibido."</p>";
+                    }
+                ?>
+                            
+            </div>
+            
+            <div class="col-lg-12">
+                <footer class="footerenvivo">
+                    <p class="textofooterenvivo">Unidad de Infraestructura DO - UCSC. Todos los derechos reservados 2021
+                        <span class="material-icons copyenvivo">
+                            copyright
+                        </span>
+                    </p>
+                    <a href="https://portal.ucsc.cl/">
+                        <label class="linkfooterenvivo">Ir a Portal Institucional</label>
+                    </a>
+                </footer>
+            </div>
+        </div>
+    </div>
+    
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  </body>
 </html>
