71533bfd99802f48adf1ebacd53f56df87a4229f
NicolasAburto <88061506+NicolasAburto@users.noreply.github.com>
2021-12-02 23:38:03 -0300

    Update envivo.php

--- Entrega 2/envivo.php
+++ Entrega 2/envivo.php
@@ -70,6 +70,8 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <title>Aforo UCSC - En vivo</title>
 
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script type="text/javascript" src="JS/funcion_datoant.js"></script>
     <meta http-equiv="Expires" content="0">
     <meta http-equiv="Last-Modified" content="0">
     <meta http-equiv="Cache-Control" content="no-cache, mustrevalidate">
@@ -111,20 +113,67 @@
                         <h4>Personas actualmente</h4>
                 </div>
             </div>
+            
             <div class="container-fluid">
+                <button onclick="mostrargrafico()" value="mostgrafico" class="btn btn-primary mb-4 botongrafico" type="button">Mostrar gráfico</button>
                 <div class="row contenedor_datosenvivo">
+                    <div id="graph">
+                        <canvas id="myChart" width="400" height="50" class="mb-5"></canvas>
+                        <script>
+                        const ctx = document.getElementById('myChart').getContext('2d');
+                        const myChart = new Chart(ctx, {
+                            type: 'bar',
+                            data: {
+                                labels: ['Capacidad total', 'Aforo máximo', 'Tiempo aforo máx.', 'Aforo mínimo', 'Tiempo aforo mín.', 'Alumnos', 
+                                'Docentes', 'Administrativos', 'Auxiliar', 'Mantención', 'Seguridad'],
+                                datasets: [{
+                                    label: 'Gráfico en vivo',
+                                    data: [<?php echo "$numerocaptotal"?> , <?php echo ""?>, <?php echo ""?>, <?php echo ""?>, <?php echo ""?>, <?php echo "$numeroalum"?>,
+                                    <?php echo "$numerodoc"?>, <?php echo "$numeroadm"?>, <?php echo "$numeroaux"?>,<?php echo "$numeroman"?>, <?php echo "$numeroseg"?>],
+                                    backgroundColor: [
+                                        'rgba(255, 99, 132, 0.2)',
+                                        'rgba(54, 162, 235, 0.2)',
+                                        'rgba(255, 206, 86, 0.2)',
+                                        'rgba(75, 192, 192, 0.2)',
+                                        'rgba(153, 102, 255, 0.2)',
+                                        'rgba(255, 159, 64, 0.2)'
+                                    ],
+                                    borderColor: [
+                                        'rgba(255, 99, 132, 1)',
+                                        'rgba(54, 162, 235, 1)',
+                                        'rgba(255, 206, 86, 1)',
+                                        'rgba(75, 192, 192, 1)',
+                                        'rgba(153, 102, 255, 1)',
+                                        'rgba(255, 159, 64, 1)'
+                                    ],
+                                    borderWidth: 1
+                                }]
+                            },
+                            options: {
+                                scales: {
+                                    y: {
+                                        beginAtZero: true
+                                    }
+                                }
+                            }
+                        });
+                        </script>
+                    </div>
+                    
                     <div class="col-lg-3 col-md-5 col-sm-7">
-                        <h3>Capacidad total</h3>
-                        <h3>Aforo máximo</h3>
-                        <h3>Tiempo aforo máx.</h3>
-                        <h3>Alumnos</h3>
-                        <h3>Docentes</h3>
-                        <h3>Administrativos</h3>
+                        
+                            <h3>Capacidad total</h3>
+                            <h3>Aforo máximo</h3>
+                            <h3>Tiempo aforo máx.</h3>
+                            <h3>Alumnos</h3>
+                            <h3>Docentes</h3>
+                            <h3>Administrativos</h3>
+                        
                     </div>
                     <div class="col-lg-3 col-md-4 col-sm-5">
                         <h3><?php echo "$numerocaptotal"?> Personas</h3>
-                        <h3><?php echo "$numeroalum"?> Personas</h3>
-                        <h3><?php echo "$numeroalum"?> Personas</h3>
+                        <h3><?php echo ""?> Personas</h3>
+                        <h3><?php echo ""?> Minutos</h3>
                         <h3><?php echo "$numeroalum"?> Personas</h3>
                         <h3><?php echo "$numerodoc"?> Personas</h3>
                         <h3><?php echo "$numeroadm"?> Personas</h3>
@@ -137,8 +186,8 @@
                         <h3>Seguridad</h3>
                     </div>
                     <div class="col-lg-3 col-md-4 col-sm-5">
-                        <h3><?php echo "$numeroalum"?> Personas</h3>
-                        <h3><?php echo "$numeroalum"?> Personas</h3>
+                        <h3><?php echo ""?> Personas</h3>
+                        <h3><?php echo ""?> Minutos</h3>
                         <h3><?php echo "$numeroaux"?> Personas</h3>
                         <h3><?php echo "$numeroman"?> Personas</h3>
                         <h3><?php echo "$numeroseg"?> Personas</h3>
