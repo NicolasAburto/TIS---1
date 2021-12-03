d428e34da74f8ab5d40890547e455a74dc029e7d
NicolasAburto <88061506+NicolasAburto@users.noreply.github.com>
2021-12-02 23:37:16 -0300

    Update datosanteriores.php

--- Entrega 2/datosanteriores.php
+++ Entrega 2/datosanteriores.php
@@ -1,5 +1,6 @@
 <?php
     require("conexion.php");
+
 ?>
 
 <!DOCTYPE html>
@@ -13,6 +14,13 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="CSS/estilo.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <script type="text/javascript" src="JS/funcion_datoant.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+    <meta http-equiv="Expires" content="0">
+    <meta http-equiv="Last-Modified" content="0">
+    <meta http-equiv="Cache-Control" content="no-cache, mustrevalidate">
+    <meta http-equiv="Pragma" content="no-cache">
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -45,37 +53,57 @@
         <div class="row">
             <div class="col-lg-6 col-md-5 col-sm-4"></div>
             <div class="col-lg-6 col-md-7 col-sm-8 margen_fecha">
-                <div class="form-check d-inline-flex me-1 mb-3 mt-4">
-                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-                    <label class="form-check-label ps-1" for="flexRadioDefault1">
-                        Día
-                    </label>
-                </div>
-                <div class="form-check d-inline-flex me-1">
-                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
-                    <label class="form-check-label ps-1" for="flexRadioDefault2">
-                        Semana
-                    </label>
-                </div>
-                <div class="form-check d-inline-flex">
-                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
-                    <label class="form-check-label ps-1" for="flexRadioDefault2">
-                        Mes
-                    </label>
-                </div>
-                <br>
-                <input type="date" name="fecha_elegida">
-                <br>
-                <button type="submit" value="buscar" class="btn btn-primary ms- mt-3 mb-4 buscar"  type="button">Buscar</button>
+                <form action="datosanteriores.php" method="GET">
+                    <select name="input_nombre_edificio" class="edificios_datosant mb-2">
+                        <?php
+                        $querydatosant= "SELECT * FROM `edificio`";
+                        $resultadodatosant=mysqli_query($conexion,$querydatosant);
+                        
+                        while($row=mysqli_fetch_assoc($resultadodatosant)){
+                            $nombre_edificio_recibido = $row['nombre_edificio'];
+                            $capacidad_maxima_edificio = $row['capacidad_maxima_edificio'];
+                        ?>
+                        <option><?php echo "$nombre_edificio_recibido"?></option>
+                        <?php
+                        }
+                        ?>
+                    </select>
+                    
+                    <br>
+                    <div class="form-check d-inline-flex me-1 mb-3 mt-2">
+                        <input onclick="mostrardia()" class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                        <label class="form-check-label ps-1" for="flexRadioDefault1">
+                            Día
+                        </label>
+                    </div>
+                    <div class="form-check d-inline-flex me-1">
+                        <input onclick="mostrarsemana()" class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2">
+                        <label class="form-check-label ps-1" for="flexRadioDefault2">
+                            Semana
+                        </label>
+                    </div>
+                    <div class="form-check d-inline-flex">
+                        <input onclick="mostrarmes()" class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault3">
+                        <label class="form-check-label ps-1" for="flexRadioDefault3">
+                            Mes
+                        </label>
+                    </div>
+                    <br>
+                    <input id="dia" type="date" name="dia_elegido">
+                    <input id="semana" type="WEEK" name="semana_elegido" >
+                    <input id="mes" type="MONTH" name="mes_elegido" >
+                    <button onclick="search()" type="submit" value="buscar" class="btn btn-primary mb-4 buscar" type="button">Buscar</button>
+                </form>
             </div>
         </div>
+
         <div class="row contenedor_datosanteriores">  
             <div class="col-lg-12 col-md-12 col-sm-12">
                 <img src="CSS/logoucsc.png" alt="" class="logodatosant">
             </div>
             <div class="col-lg-6 col-md-8 col-sm-7 margendatosant">
                 <h3>Nombre edificio</h3>
-                <h3 >Capacidad edificio</h3>
+                <h3>Capacidad edificio</h3>
                 <h3>Aforo máximo</h3>
                 <h3>Tiempo aforo máx.</h3>
                 <h3>Aforo mínimo</h3>
@@ -83,13 +111,26 @@
                 <h3>Fecha</h3>
             </div>
             <div class="col-lg-6 col-md-4 col-sm-5">
-                <h3 align="center">300 Personas</h3>
-                <h3 align="center">300 Personas</h3>
+                <?php
+                    $query= "SELECT * FROM `edificio` WHERE nombre_edificio = 'Educacion'";
+                    $resultado=mysqli_query($conexion,$query);
+                    
+                    while($row=mysqli_fetch_assoc($resultado)){
+                        $nombre_edificio_sel = $row['nombre_edificio'];
+                        $capacidad_maxima_sel = $row['capacidad_maxima_edificio'];
+                  
+
+                ?>
+                <h3 align="center"><?php echo "$nombre_edificio_sel"?></h3>
+                <?php
+                    }
+                ?>
+                <h3 align="center"> Personas</h3>
                 <h3 align="center">--cantidad--</h3>
                 <h3 align="center">--cantidad--</h3>
                 <h3 align="center">--cantidad--</h3>
                 <h3 align="center">--cantidad--</h3>
-                <h3  align="center" name="fecha_elegida" value="fecha_elegida"></h3>
+                <h3 align="center">asd</h3>
             </div>
             <a align="center" href="#" class="boton mt-5">
                 <button value="imprimir" class="btn btn-primary" type="button">Imprimir</button>
