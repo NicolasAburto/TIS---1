4e9832137fb220f57ba621d68bcf3cd50a571512
NicolasAburto <88061506+NicolasAburto@users.noreply.github.com>
2021-12-20 17:27:55 -0300

    Update acceso.php

--- Entrega 3/acceso.php
+++ Entrega 3/acceso.php
@@ -31,6 +31,10 @@
                     <a class="nav-link navbarlinks" href="datosanteriores.php">Datos anteriores</a>
                 </li>
 
+                <li class="nav-item">
+                    <a class="nav-link navbarlinks" href="faq.php">FAQ</a>
+                </li>
+
                 <a href="acceso.php">
                     <span class="material-icons iconousuario text-black">
                         account_circle
@@ -39,21 +43,31 @@
             </ul>
         </div>
     </nav>
-    <form action="funcion_acceder.php" method="POST">
-        <div class="fondoacceso">
-            <div class="container contenedor-login">
-                <div class="col">
-                    <div class="row margen">
-                        <h2>Acceso UCSC</h2>
-                            <h5 class="mt-2 mb-2">Run usuario</h5>
-                            <input name="input_user" type="text" required>
-                            <button type="submit" value="acceder" class="btn btn-primary mt-4" type="button">Acceder</button>
-                        <!-- <a align="center" href="index_manten.php"><button  class="btn btn-primary" type="button">Acceder</button></a> -->
-                    </div>
+
+    <div class="container-fluid fondoacceso">
+        <div class="row">
+            <div class="col-lg-5 col-md-12 col-sm-12">
+                <div class="acceso_contenedor_info">
+                    <h2 class="acceso_info_titulo">Información</h2>
+                    <h2 class="acceso_info">En esta sección podrás gestionar el personal y los edificios de la universidad <br><br><strong>ATENCIÓN:</strong><br> Solo puedes ingresar si eres "Administrativo"</h2>
                 </div>
             </div>
+            
+            <div class="col-lg-7 col-md-12 col-sm-12">
+                <form action="funcion_acceder.php" method="POST">
+                    <div class="contenedor-login">
+                        <h2 class="acceso_titulo">Iniciar sesión</h2>
+                        <h2 class="acceso_subtitulo">Administrador</h2>
+                        <h5 class="acceso_margen">Run usuario <strong class="acceso_run_info">(Sin puntos, ni guion)</strong></h5>
+                        <input class="acceso_margen acceso_input" name="input_user" type="text" placeholder="123456789" required>
+                        <br>
+                        <button type="submit" value="acceder" class="btn btn-primary mt-4 bg-black acceso_margen acceso_input" type="button">Acceder</button>
+                    </div>
+                </form>
+            </div>
         </div>
-    </form>
+    </div>
+   
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
